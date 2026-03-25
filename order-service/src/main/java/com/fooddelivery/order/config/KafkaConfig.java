10293403cc2b018d5bdd7c9d4abd66adbf3e487b
Shashank3025 <shashankreddy3025@gmail.com>
2026-03-24 21:53:36 -0500

    deploying kuberenetes

--- order-service/src/main/java/com/fooddelivery/order/config/KafkaConfig.java
+++ order-service/src/main/java/com/fooddelivery/order/config/KafkaConfig.java
@@ -18,7 +18,7 @@ public class KafkaConfig {
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> config = new HashMap<>();
 
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "host.docker.internal:29092");
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.ACKS_CONFIG, "all");
